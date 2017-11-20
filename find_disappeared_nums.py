6ea4ab0c975d7b2fa388429fb478fcf348b5fa18
hackk <meiyusheng246@163.com>
2017-11-20 19:47:20 +0800

    448 clever idea

--- find_disappeared_nums.py
+++ find_disappeared_nums.py
@@ -3,9 +3,10 @@ class Solution:
         """
         :type nums: List[int]
         :rtype: List[int]
+
         """
         for i in range(len(nums)):
             index = abs(nums[i]) - 1
             nums[index] = - abs(nums[index])
         return [x + 1 for x in range(len(nums)) if nums[x] > 0]
-        
+

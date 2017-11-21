e758c95db75c13d8f72fe21621df0cd02cd2e243
hackk <meiyusheng246@163.com>
2017-11-21 13:12:09 +0800

    171 take a look

---
+++ EXCEL_SHEET_COLUMN_NUMBER.py
@@ -0,0 +1,14 @@
+class Solution:
+    def titleToNumber(self, s):
+        '''
+        :type s: str
+        :rtype: int
+        '''
+        result = 0
+        for i in range(len(s)):
+            result = result * 26 + ord(s[i]) - ord('A') +1
+
+        return result
+
+
+# Take a look...

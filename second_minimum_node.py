40f319da9e151fc06f9ae946b6fa29b6227cbb6e
hackk <meiyusheng246@163.com>
2017-11-23 14:12:35 +0800

    671 easy F

---
+++ second_minimum_node.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findSecondMinimumValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        res = [float("inf")]
+        def traverse(node):
+            if node == None:
+                return 
+            if root.val < node.val < res[0]:
+                res[0] = node.val
+            traverse(node.left)
+            traverse(node.right)
+        traverse(root)
+        return -1 if res[0] == float('inf') else res[0]

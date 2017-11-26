5281e5d69520dbcde9a9afa86ff6d4fe2ad054ab
hackk <meiyusheng246@163.com>
2017-11-26 16:05:15 +0800

    477 easy fail

---
+++ total_hamming_distance.py
@@ -0,0 +1,12 @@
+class Solution:
+    def total_hamming_distance(self, nums):
+        '''
+        :type List[int]
+        :rtype int
+        '''
+        bits = [[0,0] for _ in range(32)]
+        for x in nums:
+            for i in range(32):
+                bits[i][x%2] += 1
+                x = x // 2
+        return sum(x*y for x, y in bits)

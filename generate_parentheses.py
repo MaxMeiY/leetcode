2acc675ec68125379760c47776a64ef80564037d
hackk <meiyusheng246@163.com>
2017-12-25 15:18:05 +0800

    f medium 22

---
+++ generate_parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+        def backtrack(s='', left=0, right=0):
+            if len(s) == 2*n:
+                ans.append(s)
+                return
+            if left < n:
+                backtrack(s+'(', left+1, right)
+            if right < left:
+                backtrack(s+')', left, right)
+        backtrack()
+        return ans
